a43dd3cb062d2ae4e8d1a9cd0514eaa6f31562f8
Sebastian Vetter <seb@roadsi.de>
2017-11-10 13:26:33 -0700

    Initial setup of the project

---
+++ README.rst
@@ -0,0 +1,14 @@
+pycon-double-click
+#############################
+
+
+.. image:: https://travis-ci.org/elbaschid/pycon-double-click.svg?branch=master
+   :target: https://travis-ci.org/elbaschid/pycon-double-click
+
+
+License
+-------
+
+This code is licensed under the `MIT License`_.
+
+.. _`MIT License`: https://github.com/elbaschid/pycon-double-click/blob/master/LICENSE
---
+++ setup.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import sys
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+requires = ['click']
+tests_require = ['pytest', 'pytest-cache', 'pytest-cov']
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+
+setup(
+    name="pycon-double-click",
+    version='0.0.0',
+    description="Example CLI for PyCon click talk.",
+    long_description="\n\n".join([open("README.rst").read()]),
+    license='MIT',
+    author="Sebastian Vetter",
+    author_email="seb@roadsi.de",
+    url="https://pycon-double-click.readthedocs.org",
+    packages=find_packages(),
+    install_requires=requires,
+    entry_points={'console_scripts': [
+        'forecast = forecast.cli:main']},
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython'],
+    extras_require={'test': tests_require},
+    cmdclass={'test': PyTest})
