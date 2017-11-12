5d5094a3cb10613723aab56c09643bfdc8ecc01a
Sebastian Vetter <seb@roadsi.de>
2017-11-12 14:19:48 -0700

    Add test for creating config file

--- forecast/tests/test_main.py
+++ forecast/tests/test_main.py
@@ -1,7 +1,10 @@
 import pytest
 import click
 
+from click.testing import CliRunner
+
 from forecast.cli import validate_api_key
+from forecast.cli import main
 
 
 @pytest.mark.parametrize('value', [None, 12])
@@ -11,5 +14,26 @@ def test_validate_api_key(value):
 
 
 def test_valid_api_key_validation():
-    key = '00ee52f44f3350c73f2684a0f23f2805'
+    key = '00ee52f44f3350c73f2684a0f23f2805'  # not a real API key
     assert validate_api_key(key) == key
+
+
+def test_writing_config_file():
+    runner = CliRunner()
+
+    with runner.isolated_filesystem() as env:
+        api_key = '00ee52f44f3350c73f2684a0f23f2805'
+
+        filename = f'{env}/forecast.cfg'
+
+        result = runner.invoke(
+            main,
+            ['--config-file', filename, 'config'],
+            input=api_key,
+        )
+
+        assert result.exit_code == 0, result.output
+        assert api_key in result.output
+
+        with open(filename) as cfg_file:
+            assert cfg_file.read() == api_key
